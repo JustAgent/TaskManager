cc69b54f014566fcbbd9be488436e295c398ff16
JustAgent <baykov.ivan.03@gmail.com>
2022-08-23 17:14:58 +0300

    range error fix

--- lib/model/local_storage.dart
+++ lib/model/local_storage.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:task_manager/model/model.dart';
 
 List<Product> products = [
-  Product(title: 'title1', desc: 'desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1'),
+  Product(title: 'title2', desc: 'desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1desc1'),
   Product(title: 'title2', desc: 'desc2'),
-  Product(title: 'title3', desc: 'desc3'),
-  Product(title: 'title4', desc: 'desc4'),
+  Product(title: 'title3title3title3title3title3title3title3title3title3title3', desc: 'desc3'),
+  Product(title: 'title4', desc: 'desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4'),
+  Product(title: 'title4', desc: 'desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4'),
+  Product(title: 'title4', desc: 'desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4'),
+  Product(title: 'title4', desc: 'desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4'),
+  Product(title: 'title4', desc: 'desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4'),
+  Product(title: 'title4', desc: 'desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4desc4'),
 ];
--- lib/pages/notes.dart
+++ lib/pages/notes.dart
@@ -15,73 +15,91 @@ class Notes extends StatefulWidget {
 
 class _NotesState extends State<Notes> {
 
+  late ScrollController _scrollController;
 
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _scrollController = new ScrollController(initialScrollOffset: 5.0)
+  //     ..addListener(_scrollListener);
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/addNote');
-          },
-          elevation: 20,
-          child: Container(
-            height: 60,
-            width: 60,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(begin: Alignment.bottomRight,end: Alignment.topLeft, colors: btnGradient),
-            ),
-            child: Icon(
-              Icons.add_outlined,
-              size: 30,
-            ),
+    return Scaffold(
+      //backgroundColor: Colors.red,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, '/addNote');
+        },
+        elevation: 20,
+        child: Container(
+          height: 60,
+          width: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(begin: Alignment.bottomRight,end: Alignment.topLeft, colors: btnGradient),
+          ),
+          child: Icon(
+            Icons.add_outlined,
+            size: 30,
           ),
         ),
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 0,
-          backgroundColor: Color(bgMain),
-          actions: [
-            IconButton(onPressed: () {},
-                icon: Icon(
-                    Icons.search_outlined,
-                    color: Color(appBarTextCOLOR),
-                )
-            )
-          ],
-          leading: IconButton(
-            icon: Icon(
-              Icons.menu,
-              color: Color(appBarTextCOLOR),
-            ),
-            onPressed: () {  },
-          ),
-          title: Text(
-            'Recent Notes',
-            style: GoogleFonts.nunito(
-              fontWeight: FontWeight.w900,
-              color: Color(appBarTextCOLOR),
-              fontSize: 14,
-            ),
-          ),
-        ),
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
         backgroundColor: Color(bgMain),
-        body: Center(
-          child: Expanded(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(22, 0, 22, 0),
-              child: StaggeredGrid.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 12,
-                mainAxisSpacing: 8,
-                children:
-                  products.map((product) => Note(() {} , product)).toList(),
-              ),
-            ),
+        actions: [
+          IconButton(onPressed: () {},
+              icon: Icon(
+                  Icons.search_outlined,
+                  color: Color(appBarTextCOLOR),
+              )
           )
+        ],
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Color(appBarTextCOLOR),
+          ),
+          onPressed: () {  },
         ),
+        title: Text(
+          'Recent Notes',
+          style: GoogleFonts.nunito(
+            fontWeight: FontWeight.w900,
+            color: Color(appBarTextCOLOR),
+            fontSize: 14,
+          ),
+        ),
+      ),
+      backgroundColor: Color(bgMain),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            minHeight: 400,
+          ),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(22, 0, 22, 0),
+            child: MasonryGridView.count(
+              physics: const AlwaysScrollableScrollPhysics(),
+              //controller: _scrollController,
+              crossAxisCount: 2,
+              scrollDirection: Axis.vertical,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 8,
+              itemBuilder: (BuildContext context, int index) {
+                if (index < products.length) {
+                    return Note(() {}, products[index]);
+                }
+                return const Padding(padding: EdgeInsets.all(0));
+
+              },
+                // products.map((product) => Note(() {} , product)).toList();
+            ),
+          ),
+        )
       ),
     );
   }
